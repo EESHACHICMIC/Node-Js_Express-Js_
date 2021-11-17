ba8d818e3ad85c594f2a1ce90dfcb06b8046235c
prateekjain9997 <mdeesha1996@gmail.com>
2021-11-17 13:36:53 +0530

    added Project 4 and inserted data using post request with the help of node js and expresss

--- Project_04/server.js
+++ Project_04/server.js
@@ -43,37 +43,27 @@ const stuListSchema = new mongoose.Schema({
 const StudentList = new mongoose.model("StudentList", stuListSchema);
 
 app.post('/api/student', (req, res) => {
-    const stuData = {
-        name: req.body.name,
-        class: req.body.class,
-        college: req.body.college,
-        marks: req.body.marks,
-        active: req.body.active
+
+    const createDocument = async () => {
+        try {
+            const stuData = {
+                name: req.body.name,
+                class: req.body.class,
+                college: req.body.college,
+                marks: req.body.marks,
+                active: req.body.active
+            }
+            const stu = new StudentList(stuData);
+            const result = await stu.save();
+            console.log(result)
+            
+            res.json(stuData);
+        } catch (error) {
+            console.log(`error is generated`,error);
+        }
     }
-    console.log(stuData);
-    const stu = new StudentList(stuData);
-    stu.save();
-    res.json(stuData);
+    createDocument();
+
 })
-
-//create document
-
-// const stu = new StudentList({
-//     name: "Alok Raj",
-//     class: "MCA",
-//     college: "LPU",
-//     marks: 320,
-//     active: true,
-
-// })
-
-
-
-
-
-app.get('/api/student', (req, res) => {
-    // res.json(stu)
-})
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
